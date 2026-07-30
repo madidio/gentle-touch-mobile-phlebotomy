932cd7641b0f8799b6ddee7f5211c02a0d702af8
madidio <gentletouchlabs27@gmail.com>
2026-07-30 14:18:06 -0500

    Fix AboutHero import path casing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/sections/Hero";
 import Services from "@/components/sections/Services";
-import AboutHero from "@/components/sections/About/AboutHero";
+import AboutHero from "@/components/sections/about/AboutHero";
 import Footer from "@/components/sections/Footer";
 
 function Home() {
